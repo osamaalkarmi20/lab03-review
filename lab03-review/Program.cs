8ab892385f6a89fa9523a01ce0514213876e0bb4
osamaalkarmi20 <osamaalkarmi20.outlook@outlook.com>
2023-06-19 14:14:27 +0300

    Add project files.

---
+++ lab03-review/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.IO;
+
+namespace lab03_review
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
